d59130f154d1f1878a6d9a0429ed4be3b0c945be
Machine gun <call_from_helheim@protonmail.com>
2023-09-16 00:08:36 -0300

    first commit

---
+++ Pyramid/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace Pyramid
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(275, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(42, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(15, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Pyramid Cheat Tool Trainer";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button2.Location = new System.Drawing.Point(11, 310);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(305, 57);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Regenerate Health";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button3.Location = new System.Drawing.Point(10, 526);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(306, 57);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Set Weapon";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.Black;
+            this.comboBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Chainsaw",
+            "Revolver",
+            "Great Knife"});
+            this.comboBox1.Location = new System.Drawing.Point(10, 499);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.comboBox1.Size = new System.Drawing.Size(306, 21);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Pyramid.Properties.Resources._381_3814276_pirates_logo_vector_pirate_clip_art_hd_png_copia1;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(11, 35);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(305, 269);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Transparent;
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button4.Location = new System.Drawing.Point(11, 373);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(306, 57);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "All Maps - All Keys";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Transparent;
+            this.button5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button5.Location = new System.Drawing.Point(10, 436);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(306, 57);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Full Inventory James";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(328, 591);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Pyramid/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Memory;
+using System.Threading;
+
+namespace Pyramid
+{
+    public partial class Form1 : Form
+    {
+        public Mem m = new Mem();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            m.OpenProcess("sh2pc.exe");
+        }
+
+        // BUTTON WAS CLICKED
+
+        private bool button2WasClicked = true;
+        private bool button3WasClicked = true;
+        private bool button4WasClicked = true;
+
+        // BUTTONS FUNCTIONS
+
+
+        private void button1_Click(object sender, EventArgs e) // button close
+        {
+            this.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e) // set weapon button
+        {
+            // 01F7A809 inventory 
+
+            if (comboBox1.SelectedItem == "Chainsaw")
+            {
+                m.WriteMemory("01F7A809", "bytes", "10");
+            }
+            else if (comboBox1.SelectedItem == "Revolver")
+            {
+                m.WriteMemory("01F7A809", "bytes","0A");
+            }
+            else if (comboBox1.SelectedItem == "Great Knife")
+            {
+                m.WriteMemory("01F7A809", "bytes", "0F");
+            }
+            else
+            {
+                MessageBox.Show("Wrong Option, try again nigger");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) // button god mode
+        {
+                button2.ForeColor = Color.Green;
+                m.WriteMemory("01FB113C", "float", "100");
+            MessageBox.Show("The health has been regerate sucefully!");
+            Thread.Sleep(2);
+                button2.ForeColor = Color.Red;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) // COMBO BOX 
+        {
+            
+        }
+
+        // CREDITS.
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Credits to MACHINE-GUN-SADOMY!");
+        }
+
+        private void button4_Click(object sender, EventArgs e) // Have All keys and etc
+        {
+            m.WriteMemory("01F7A7E3", "bytes", "FF FF FF FF FF"); // KEYS
+            m.WriteMemory("01F7A9E0", "bytes", "00 1C 00 FF"); // MAPS
+            MessageBox.Show("The all keys and maps are ready, check the inventory.");
+            // HAVE ALL KEYS 01F7A7E3 FF FF FF FF FF
+            // HAVE ALL MAPS 01F7A9E0 00 1C 00 FF
+        }
+
+        private void button5_Click(object sender, EventArgs e) // Full inventory
+        {
+            // HAVE ALL INVETORY
+            m.WriteMemory("01F7A7DF", "bytes", "FE F3 FD");
+            // ammo
+            m.WriteMemory("01F7A7F0", "bytes", "03 00 00 00 00 00 2C 01 00 00 2C 01 00 00 2C 01");
+            m.WriteMemory("01F7A802", "bytes", "2C 01");
+            // first aid
+            // health drinks
+            // ampouts
+        }
+    }
+}
+    
